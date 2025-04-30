2d516c00bf6ad2cc54a7f430d966828b4037b08e
Frederico Ackermann Cera <73366611+fredcera@users.noreply.github.com>
2025-04-30 21:43:08 +0000

    Atualização do Readme.md

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Obrigado por ser parte do que hoje faz toda a diferença na minha vida!</h1>
+    return <h1>Teste1! Obrigado por ser parte do que hoje faz toda a diferença na minha vida!</h1>
 }
 
 export default Home;
