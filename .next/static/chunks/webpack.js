2d70f4cbaeef8fcb2cbdbec3f7134a4e1f0b8352
Frederico Ackermann Cera <73366611+fredcera@users.noreply.github.com>
2025-05-22 00:42:12 +0000

    Correção do bug ao abrir no navegador mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a306a0e99fff110c"; }
+/******/ 		__webpack_require__.h = function() { return "985e93ce1618376c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
